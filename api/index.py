33e2c9772c6f775fc323a795cf97d0259bc3115a
nao20010128nao <nao20010128@gmail.com>
2020-05-20 07:48:19 +0000

    YTDLのバージョン

--- api/index.py
+++ api/index.py
@@ -1,10 +1,14 @@
 from youtube_dl import YoutubeDL
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def catch_all(path):
+def root(path):
   url=request.args.get('url') or path
   info=YoutubeDL({}).extract_info(url, download=False)
   return info
+
+@app.route('/version')
+def version():
+  return jsonify(youtube_dl.version.__version__)
