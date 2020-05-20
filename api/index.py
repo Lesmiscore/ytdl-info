17a51f97665d342d212c4fc769f1c5c5bbbb5006
nao20010128nao <nao20010128@gmail.com>
2020-05-20 07:10:57 +0000

    最初

---
+++ api/index.py
@@ -0,0 +1,10 @@
+from youtube_dl import YoutubeDL
+from flask import Flask, Response, request
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+  url=request.args.get('url') or path
+  info=YoutubeDL({}).extract_info(url, download=False)
+  return Response(info)
